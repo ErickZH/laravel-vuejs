40e82df35f0e3bdacdec1af3f6055eb34e48ba02
ErickZH <zaraterick@outlook.com>
2018-01-16 14:29:26 -0600

    Implementacion de la eliminacion de tareas.

--- public/js/app.js
+++ public/js/app.js
@@ -12415,7 +12415,15 @@ new Vue({
 		{
 			var urlKeeps = 'tasks';
 			axios.get(urlKeeps).then(response => {
-				this.keeps = response.data;
+				this.keeps = response.data
+			});
+		},
+		deleteKeep: function(keep)
+		{
+			var url = 'tasks/' + keep.id;
+			
+			axios.delete(url).then(response => {
+				this.getKeeps();
 			});
 		}
 	}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 		</div>
 		<div class="col-sm-7">
 			<a href="#" class="btn btn-primary pull-right">Nueva tarea</a>
-			<table class="table table-hover table-sprite">
+			<table class="table table-hover table-striped">
 				<thead>
 					<th>#</th>
 					<th>Tarea</th>
@@ -23,7 +23,7 @@
 							<a href="#" class="btn btn-warning btn-sm">Editar</a>
 						</td>
 						<td width="10px">
-							<a href="#" class="btn btn-danger btn-sm">Eliminar</a>
+							<a class="btn btn-danger btn-sm" v-on:click.prevent="deleteKeep(keep)">Eliminar</a>
 						</td>
 					</tr>
 				</tbody>
